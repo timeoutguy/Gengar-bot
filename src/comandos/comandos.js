af75a6f6a8373177dd4e78681c11c0ba293e0f52
timeoutguy <marcusvyny92@gmail.com>
2019-12-12 23:30:39 -0300

    Criei os comandos básicos e mensagem de boa vinda

---
+++ src/comandos/comandos.js
@@ -0,0 +1,11 @@
+module.exports = {
+    abortar: function (msg) {
+        msg.reply('Abortar é bom d++++')
+    },
+    ancap: function (msg) {
+        msg.reply('Todo ancap é incel')
+    },
+    imposto: function (msg) {
+        msg.reply('É roubo')
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,52 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js'); //Importando discord.js
+const client = new Discord.Client(); //Iniciando o client
 
-client.on('ready', () => {
+const login = require('./login.json'); //Importando o token de login do bot
+
+client.login(login.token); //fazendo login no client
+
+//Objeto com os comanndos do bot
+const comandos = {
+  gado(msg) {
+    msg.reply('Para ver meus comandos digite "gado_help"');
+    let desc = "Exibe a lista de comandos";
+  },
+  gado_abortar(msg) {
+    msg.reply('Abortar é bom d++++');
+    let desc = "Opinião do bot sobe abortar";
+  },
+  gado_ancap(msg) {
+    msg.reply('Todo ancap é incel');
+    let desc = "Opinião do bot sobe os ancaps"
+  },
+  gado_imposto(msg) {
+    msg.reply('É roubo')
+    let desc = "Opinião do bot sobe imposto"
+  },
+}
+
+//Quando o bot estiver pronto ele era imprimir uma mensagem no console
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content.toUpperCase()  === 'IMPOSTO') {
-    msg.reply('É necessário para que a sociedade se mantenha!');
+//Mensagem de boas vindas
+client.on('guildMemberAdd', member => {
+  let channel = member.guild.channels.find(ch => ch.name === 'bem-vindo');
+
+  channel.send(`Bem vindo ao servidor, ${member}. Para ver meus comandos digite "gado_help"`);
+})
+
+//Recebe mensagem do usuario
+client.on("message", msg => {
+  let messageContent = msg.content; //Armazenando o conteudo da mensagem
+  let command = comandos[messageContent.toLowerCase()]; //Buscando o comando no objeto
+
+  //Verificando se o comando existe
+  if (command) {
+    command(msg)
   }
-  if (msg.content.toUpperCase()  === 'ANCAP') {
-      msg.reply('Todo ancap é incel!');
-  }
-  if (msg.content.toUpperCase()  === 'LIVROS'){
-      msg.reply('Lista de leituras para o libertário iniciante https://bunkerlibertario.com/lista/lista-de-leituras-para-o-libertario-iniciante/')
-  }
-  if (msg.content.toUpperCase() === 'ABORTAR'){
-      msg.reply('Abortar é bom d++++')
+  if (messageContent === "gado_help") {
+    //USAR FOR IN PARA LISTAR OS COMANDOS
   }
 });
-
-console.log(client);
-client.login('NjA2OTY4OTc4NTg5NjE0MDgw.XUSzIg.qpKgA_nqcberhrjtP_D-nCrjydk');
