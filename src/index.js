b19b578a572275dc85065ee40ed34498f13cd403
Marcus <marcusvyny92@gmail.com>
2020-04-01 18:45:45 -0300

    Handle commands created

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ const Discord = require('discord.js'); //Importando discord.js
 const client = new Discord.Client(); //Iniciando o client
 
 const config = require('./config.json'); //Importando o token de login do bot
-const commands = require('./commands/comandos') //Objeto com os comanndos do bot
 
 const prefix = config.prefix; // Prefixo
-const active = new Map();
 
 client.login(config.token); //fazendo login no client
 
@@ -15,7 +13,7 @@ client.on("ready", () => {
 
   client.user.setPresence({
     status: "online",
-    game:{
+    game: {
       name: "Estou comendo o cu de quem ta lendo",
       type: "PLAYING"
     }
@@ -30,21 +28,22 @@ client.on("message", msg => {
 
   const command = args.shift().toLowerCase()
 
-  if(!commands[command]){
-    const embed = new Discord.RichEmbed()
-      .setTitle(":x: Erro")
-      .setColor("#7510f7")
-      .setDescription(`O comando ${command} não existe`);
+  if (msg.content.startsWith(prefix)) {
 
-    msg.channel.send(embed)
+    try {
 
-    return;
+      let Path = require('./commands/' + command + ".js")
+      return Path.run(client, msg, args)
+
+    } catch (err) {
+
+      const embed = new Discord.RichEmbed()
+        .setTitle(":x: Erro")
+        .setColor("#7510f7")
+        .setDescription(`O comando ${command} não existe`);
+
+      return msg.channel.send(embed).then(msg.delete());
+    }
+
   }
-
-  let ops = {
-    active: active
-  }
-
-  commands[command].run(client, msg, args, ops)
-  
 });
